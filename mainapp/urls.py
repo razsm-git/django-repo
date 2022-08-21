a7c9511e9e68d1c8341312e17efbe2e799348b06
test_name <test@example.com>
2022-08-21 15:37:25 +0000

    lesson_2

---
+++ mainapp/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+from mainapp import views
+from mainapp.apps import MainappConfig
+
+app_name = MainappConfig.name
+
+urlpatterns = [
+    path("", views.MainPageView.as_view(), name="home"),
+    path("index/", views.MainPageView.as_view(), name="home"),
+    path("news/", views.NewsPageView.as_view(), name="news"),
+    path("courses/", views.CoursesPageView.as_view(), name="courses"),
+    path("contacts/", views.ContactsPageView.as_view(), name="contacts"),
+    path("doc_site/", views.DocSitePageView.as_view(), name="doc_site"),
+    path("login/", views.LoginPageView.as_view(), name="login"),
+]
