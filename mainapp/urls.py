2f9f5d7b6c96c2ce87376157525ae205e8e46a63
test_name <test@example.com>
2022-09-03 11:58:56 +0000

    lesson_3 Templates including

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path("", views.MainPageView.as_view(), name="home"),
     path("index/", views.MainPageView.as_view(), name="home"),
     path("news/", views.NewsPageView.as_view(), name="news"),
+    path("news/<int:page>/", views.NewsWithPaginatorView.as_view(), name="news_paginator"),
     path("courses/", views.CoursesPageView.as_view(), name="courses"),
     path("contacts/", views.ContactsPageView.as_view(), name="contacts"),
     path("doc_site/", views.DocSitePageView.as_view(), name="doc_site"),
